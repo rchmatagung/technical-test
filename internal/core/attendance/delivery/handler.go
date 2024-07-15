b70580f3b0aa5017962985bd5919b7f62b83e558
rchmatagung <rachmatagung003691@gmail.com>
2024-07-15 12:57:51 +0700

    feat: attendance service and set config

--- internal/core/attendance/delivery/handler.go
+++ internal/core/attendance/delivery/handler.go
@@ -2,7 +2,16 @@ package delivery
 
 import (
 	"boilerplate/config"
+	"boilerplate/internal/core/attendance/models"
 	"boilerplate/internal/wrapper/usecase"
+	"boilerplate/pkg/exception"
+	"context"
+	"fmt"
+	"strconv"
+
+	cm "boilerplate/pkg/constants/message"
+
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,4 +27,102 @@ func NewAttendanceHandler(uc usecase.Usecase, conf *config.Config, logger *logru
 		Conf:    conf,
 		Log:     logger,
 	}
+}
+
+func (a AttendanceHandler) AttendanceAbsentIn(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	dataReq := new(models.CreateAttendanceAbsentInRequest)
+	if err := c.BodyParser(dataReq); err != nil {
+		return exception.CreateResponse_Log(init, fiber.StatusBadRequest, cm.ErrBodyParser, cm.ErrBodyParserInd, nil)
+	}
+
+	username := fmt.Sprintf("%v", c.Locals("employee_name"))
+
+	respData, errData := a.Usecase.Core.Attendance.AbsentIn(context.Background(), dataReq, username)
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+
+	return exception.CreateResponse_Log(init, fiber.StatusCreated, "Success create attendance absent in", "Sukses membuat absen masuk", respData)
+}
+
+func (a AttendanceHandler) AttendanceAbsentOut(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	dataReq := new(models.CreateAttendanceAbsentOutRequest)
+	if err := c.BodyParser(dataReq); err != nil {
+		return exception.CreateResponse_Log(init, fiber.StatusBadRequest, cm.ErrBodyParser, cm.ErrBodyParserInd, nil)
+	}
+
+	username := fmt.Sprintf("%v", c.Locals("employee_name"))
+
+	respData, errData := a.Usecase.Core.Attendance.AbsentOut(context.Background(), dataReq, username)
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+
+	return exception.CreateResponse_Log(init, fiber.StatusCreated, "Success create attendance absent out", "Sukses membuat absen keluar", respData)
+}
+
+func (a AttendanceHandler) GetAllAttendance(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	respData, total, errData := a.Usecase.Core.Attendance.GetAllAttendance(context.Background())
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+
+	return exception.CreateResponse_Log_Page(init, fiber.StatusOK, "Success get all attendance", "Sukses mendapatkan semua data absensi", respData, 1, 0, total)
+}
+
+func (a AttendanceHandler) GetAttendanceById(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	attendance_id, err := strconv.Atoi(c.Query("attendance_id"))
+	if err != nil {
+		return exception.CreateResponse_Log(init, fiber.StatusBadRequest, cm.ErrValuePageInt("attendance_id"), cm.ErrValuePageIntInd("attendance_id"), nil)
+	}
+
+	respData, errData := a.Usecase.Core.Attendance.GetAttendanceById(context.Background(), int64(attendance_id))
+
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+
+	return exception.CreateResponse_Log(init, fiber.StatusOK, "Success get attendance by id", "Sukses mendapatkan data absensi berdasarkan id", respData)
+}
+
+func (a AttendanceHandler) GetAttendanceByEmployeeId(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	employee_id, err := strconv.Atoi(c.Query("employee_id"))
+	if err != nil {
+		return exception.CreateResponse_Log(init, fiber.StatusBadRequest, cm.ErrValuePageInt("employee_id"), cm.ErrValuePageIntInd("employee_id"), nil)
+	}
+
+	respData, errData := a.Usecase.Core.Attendance.GetAttendanceByEmployeeId(context.Background(), int64(employee_id))
+
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+
+	return exception.CreateResponse_Log(init, fiber.StatusOK, "Success get attendance by employee id", "Sukses mendapatkan data absensi berdasarkan id karyawan", respData)
+}
+
+func (a AttendanceHandler) Delete(c *fiber.Ctx) error {
+	init := exception.InitException(c, a.Conf, a.Log)
+
+	attendance_id, err := strconv.Atoi(c.Query("attendance_id"))
+	if err != nil {
+		return exception.CreateResponse_Log(init, fiber.StatusBadRequest, cm.ErrValuePageInt("attendance_id"), cm.ErrValuePageIntInd("attendance_id"), nil)
+	}
+
+	username := fmt.Sprintf("%v", c.Locals("employee_name"))
+
+	errData := a.Usecase.Core.Attendance.Delete(context.Background(), int64(attendance_id), username)
+	if errData != nil {
+		return exception.CreateResponse_Log(init, errData.Code, errData.Message, errData.MessageInd, nil)
+	}
+	return exception.CreateResponse_Log(init, fiber.StatusOK, "Success delete attendance", "Sukses menghapus data absensi", nil)
 }
--- internal/core/attendance/usecase/usecase.go
+++ internal/core/attendance/usecase/usecase.go
@@ -2,12 +2,27 @@ package usecase
 
 import (
 	"boilerplate/config"
+	"boilerplate/internal/core/attendance/models"
 	repo "boilerplate/internal/wrapper/repository"
+	"boilerplate/pkg/exception"
 	"boilerplate/pkg/infra/db"
+	"context"
+	"fmt"
+	"time"
+
+	myvalidator "boilerplate/pkg/validator"
+
+	"github.com/gofiber/fiber/v2"
 	"github.com/sirupsen/logrus"
 )
 
 type Usecase interface {
+	AbsentIn(ctx context.Context, dataReq *models.CreateAttendanceAbsentInRequest, createdBy string) (*models.GetAttendanceScan, *exception.Error)
+	AbsentOut(ctx context.Context, dataReq *models.CreateAttendanceAbsentOutRequest, updatedBy string) (*models.GetAttendanceScan, *exception.Error)
+	GetAllAttendance(ctx context.Context) (*[]models.ListAttendanceScan, int, *exception.Error)
+	GetAttendanceById(ctx context.Context, attendanceId int64) (*models.GetAttendanceScan, *exception.Error)
+	GetAttendanceByEmployeeId(ctx context.Context, employeeId int64) (*models.GetAttendanceScan, *exception.Error)
+	Delete(ctx context.Context, attendanceId int64, deletedBy string) *exception.Error
 }
 
 type AttendanceUsecase struct {
@@ -24,4 +39,187 @@ func NewAttendanceUsecase(repository repo.Repository, conf *config.Config, dbLis
 		DBList: dbList,
 		Log:    logger,
 	}
+}
+
+func (e AttendanceUsecase) AbsentIn(ctx context.Context, dataReq *models.CreateAttendanceAbsentInRequest, createdBy string) (*models.GetAttendanceScan, *exception.Error) {
+
+	errMsg, errMsgInd := myvalidator.ValidateDataRequest(dataReq)
+
+	if errMsg != "" || errMsgInd != "" {
+		return nil, exception.NewError(fiber.StatusBadRequest, errMsg, errMsgInd)
+	}
+
+	data, err := e.Repo.Core.Attendance.GetAttendanceByEmployeeId(ctx, dataReq.EmployeeId)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if data.AttendanceId != 0 {
+		return nil, exception.NewError(fiber.StatusNotFound, fmt.Sprintf("Employee with id %v already absent in", dataReq.EmployeeId), fmt.Sprintf("Karyawan dengan id %v sudah absen masuk", dataReq.EmployeeId))
+	}
+
+	// data.absentin tidak boleh lebih dari data.absenout
+	if data.AbsentOut.After(data.AbsentIn) {
+		return nil, exception.NewError(fiber.StatusBadRequest, "Absent in cannot be greater than absent out", "Absen masuk tidak boleh lebih dari absen keluar")
+	}
+
+	params := make([]interface{}, 0)
+	params = append(params, dataReq.EmployeeId, dataReq.LocationId, time.Now(), createdBy)
+
+	attendanceId, err := e.Repo.Core.Attendance.InsertAbsentIn(ctx, params...)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if attendanceId == 0 {
+		return nil, exception.NewError(fiber.StatusInternalServerError, "Failed to create absent in", "Gagal membuat absen masuk")
+	}
+
+	respData, err := e.Repo.Core.Attendance.GetAttendanceById(ctx, attendanceId)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if respData != nil {
+		if respData.AttendanceId == 0 {
+			return nil, exception.NewError(fiber.StatusInternalServerError, "Failed to get absent in", "Gagal mendapatkan absen masuk")
+		}
+	}
+
+	return respData, nil
+}
+
+func (e AttendanceUsecase) AbsentOut(ctx context.Context, dataReq *models.CreateAttendanceAbsentOutRequest, updateBy string) (*models.GetAttendanceScan, *exception.Error) {
+
+	errMsg, errMsgInd := myvalidator.ValidateDataRequest(dataReq)
+
+	if errMsg != "" || errMsgInd != "" {
+		return nil, exception.NewError(fiber.StatusBadRequest, errMsg, errMsgInd)
+	}
+
+	data, err := e.Repo.Core.Attendance.GetAttendanceByEmployeeId(ctx, dataReq.EmployeeId)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if data.AttendanceId == 0 {
+		return nil, exception.NewError(fiber.StatusNotFound, fmt.Sprintf("Employee with id %v not found", dataReq.EmployeeId), fmt.Sprintf("Karyawan dengan id %v tidak ditemukan", dataReq.EmployeeId))
+	}
+
+	if data.AbsentIn.IsZero() {
+		return nil, exception.NewError(fiber.StatusNotFound, fmt.Sprintf("Employee with id %v not absent in", dataReq.EmployeeId), fmt.Sprintf("Karyawan dengan id %v belum absen masuk", dataReq.EmployeeId))
+	}
+
+	if data.AbsentOut.After(data.AbsentIn) {
+		return nil, exception.NewError(fiber.StatusNotFound, fmt.Sprintf("Employee with id %v already absent out", dataReq.EmployeeId), fmt.Sprintf("Karyawan dengan id %v sudah absen keluar", dataReq.EmployeeId))
+	}
+
+	params := make([]interface{}, 0)
+	params = append(params, time.Now(), updateBy, dataReq.EmployeeId, dataReq.LocationId)
+
+	attendanceId, err := e.Repo.Core.Attendance.InsertAbsentOut(ctx, params...)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if attendanceId == 0 {
+		return nil, exception.NewError(fiber.StatusInternalServerError, "Failed to create absent out", "Gagal membuat absen keluar")
+	}
+
+	respData, err := e.Repo.Core.Attendance.GetAttendanceById(ctx, attendanceId)
+
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+
+	}
+
+	if respData != nil {
+		if respData.AttendanceId == 0 {
+			return nil, exception.NewError(fiber.StatusInternalServerError, "Failed to get absent out", "Gagal mendapatkan absen keluar")
+		}
+	}
+
+	return respData, nil
+}
+
+func (e AttendanceUsecase) GetAllAttendance(ctx context.Context) (*[]models.ListAttendanceScan, int, *exception.Error) {
+
+	listAttendance, count, err := e.Repo.Core.Attendance.GetAllAttendance(ctx)
+	if err != nil {
+		return nil, 0, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if count == 0 {
+		return nil, 0, exception.NewError(fiber.StatusOK, "Success but no data found", "Sukses tetapi tidak ada data yang ditemukan")
+	}
+
+	return listAttendance, count, nil
+}
+
+func (e AttendanceUsecase) GetAttendanceById(ctx context.Context, attendanceId int64) (*models.GetAttendanceScan, *exception.Error) {
+
+	if attendanceId == 0 {
+		return nil, exception.NewError(fiber.StatusBadRequest, "Attendance id is required", "Id absen dibutuhkan")
+	}
+
+	data, err := e.Repo.Core.Attendance.GetAttendanceById(ctx, attendanceId)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if data != nil {
+		if data.AttendanceId == 0 {
+			return nil, exception.NewError(fiber.StatusNotFound, "Attendance not found", "Absen tidak ditemukan")
+		}
+	}
+
+	return data, nil
+}
+
+func (e AttendanceUsecase) GetAttendanceByEmployeeId(ctx context.Context, employeeId int64) (*models.GetAttendanceScan, *exception.Error) {
+
+	if employeeId == 0 {
+		return nil, exception.NewError(fiber.StatusBadRequest, "Employee id is required", "Id karyawan dibutuhkan")
+	}
+
+	data, err := e.Repo.Core.Attendance.GetAttendanceByEmployeeId(ctx, employeeId)
+	if err != nil {
+		return nil, exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if data != nil {
+		if data.AttendanceId == 0 {
+			return nil, exception.NewError(fiber.StatusNotFound, "Attendance not found", "Absen tidak ditemukan")
+		}
+	}
+
+	return data, nil
+}
+
+func (e AttendanceUsecase) Delete(ctx context.Context, attendanceId int64, deletedBy string) *exception.Error {
+
+	if attendanceId == 0 {
+		return exception.NewError(fiber.StatusBadRequest, "Attendance id is required", "Id absen dibutuhkan")
+	}
+
+	data, err := e.Repo.Core.Attendance.GetAttendanceById(ctx, attendanceId)
+	if err != nil {
+		return exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if data != nil {
+		if data.AttendanceId == 0 {
+			return exception.NewError(fiber.StatusNotFound, "Attendance not found", "Absen tidak ditemukan")
+		}
+	}
+
+	respData, err := e.Repo.Core.Attendance.Delete(ctx, attendanceId)
+	if err != nil {
+		return exception.NewError(fiber.StatusInternalServerError, err.Error(), err.Error())
+	}
+
+	if respData == 0 {
+		return exception.NewError(fiber.StatusInternalServerError, "Failed to delete attendance", "Gagal menghapus absen")
+	}
+	return nil
 }
