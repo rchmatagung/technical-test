ac7e7813e586aaf618eb605c1dbb4680892e1885
rchmatagung <rachmatagung003691@gmail.com>
2024-07-15 12:35:48 +0700

    feat: department endpoint

--- internal/core/department/models/request.go
+++ internal/core/department/models/request.go
@@ -1,1 +1,10 @@
 package models
+
+type CreateDepartmentRequest struct {
+	DepartmentName string `json:"department_name" validate:"required"`
+}
+
+type UpdateDepartmentRequest struct {
+	DepartmentId   int64  `json:"department_id" validate:"required"`
+	DepartmentName string `json:"department_name" validate:"required"`
+}
--- internal/core/department/repository/sql_queries.go
+++ internal/core/department/repository/sql_queries.go
@@ -1,1 +1,48 @@
 package repository
+
+const (
+	InsertDepartment = `INSERT INTO department(department_name, created_by) VALUES (?, ?)`
+
+	qReturnID = `
+	RETURNING department_id
+	`
+
+	qCount = `
+	SELECT COUNT(department_id) AS total FROM department
+	`
+
+	qSelectDepartment = `
+	SELECT department_id, department_name, created_by, created_at, updated_by, updated_at
+	FROM department
+	`
+
+	qWhere = `
+	WHERE
+	`
+
+	qDepartmentNameCaseInSensitive = `
+	LOWER(department_name) = LOWER(?)
+	`
+
+	qAnd = `
+	AND
+	`
+
+	qNotDeleted = `
+	deleted_at IS NULL
+	`
+
+	UpdateDepartment = `
+	UPDATE department
+	SET department_name = ?,
+		updated_by = ?,
+		updated_at = ?
+	WHERE department_id = ?
+	`
+
+	DeleteDepartment = `
+	UPDATE department
+	SET deleted_at = NOW()
+	WHERE department_id = ?
+	`
+)
